27bddda84eff5577ac70ad686c5414679356a39b
NepNath <neptonath@gmail.com>
2024-05-21 16:49:12 +0200

    restarted project

--- popup.js
+++ popup.js
@@ -1,52 +1,14 @@
-console.log("popup script chargé et prêt.");
-
-function fetchVideoUrl(callback) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "getVideoUrl"}, function(response) {
-            if (chrome.runtime.lastError) {
-                console.log("Erreur de communication avec le content script:", chrome.runtime.lastError.message);
-                callback(null);
-            } else if (response) {
-                console.log('URL récupérée :', response.url);  // Log de l'URL récupérée
-                callback(response.url);
-            } else {
-                console.log("Aucune réponse du content script.");
-                callback(null);
-            }
-        });
-    });
-}
-
-function fetchVideoInfo(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: "getVideoDetails"}, function(response) {
-            if (response) {
-                document.getElementById('videoTitle').textContent = response.title;
-                console.log("titre récupéré : ", response.title)
-                document.getElementById('videoURL').textContent = response.url;
-            }
-        });
-    });
-}
-
-document.getElementById('download-mp3').addEventListener('click', function() {
-    fetchVideoUrl(function(url) {
-        if (url) {
-            console.log('Téléchargement MP3 de : ' + url);
-            // Logique de téléchargement MP3 ici
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('download-mp4').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let url = tabs[0].url;
+        if (url.includes("youtube.com")) {
+          // Ici, tu pourras appeler l'API ou la méthode de téléchargement
+          alert("URL YouTube détectée : " + url);
         } else {
-            console.log('Aucune vidéo valide trouvée.');
+          alert("L'URL n'est pas une URL YouTube.");
         }
+      });
     });
-});
-
-document.getElementById('download-mp4').addEventListener('click', function() {
-    fetchVideoUrl(function(url) {
-        if (url) {
-            console.log('Téléchargement MP4 de : ' + url);
-            // Logique de téléchargement MP4 ici
-        } else {
-            console.log('Aucune vidéo valide trouvée.');
-        }
-    });
-});
+  });
+  
