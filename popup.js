348b245f313913dad589762b0c48d65a0166d2d9
NepNath <neptonath@gmail.com>
2024-05-21 17:00:11 +0200

    edit on new project (fully fonctionnal)

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('download-mp4').addEventListener('click', function() {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         let url = tabs[0].url;
-        if (url.includes("youtube.com")) {
-          // Ici, tu pourras appeler l'API ou la méthode de téléchargement
+        if (url.includes("youtube.com/watch?v=")) {
+          // appeler API pour télécharger
           alert("URL YouTube détectée : " + url);
         } else {
           alert("L'URL n'est pas une URL YouTube.");
@@ -11,4 +11,19 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('download-mp3').addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let url = tabs[0].url;
+        if (url.includes("youtube.com/watch?v=")) {
+          //appeler API pour télécharger
+          alert("URL YouTube détectée : " + url);
+        } else {
+          alert("L'URL n'est pas une URL YouTube.");
+        }
+      });
+    });
+  });
+  
   
