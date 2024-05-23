f6a8f45886c52d7d00838ebef4097e89a55256f5
NepNath <115375139+NepNath@users.noreply.github.com>
2024-05-23 15:49:48 +0200

    inchallah it works

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const youtubedl = require('youtube-dl-exec');
-const cors =require('cors');
-const { copyFileSync } = require('fs');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
 
 app.use(cors({
-    origin: 'chrome-extension://dohbggmomelggkmmegdmmhjmbepdbfmd'
+  origin: 'chrome-extension://opnlomakpnoffilmacbapoefgplmjoab'
 }));
 
 app.get('/download-mp4', (req, res) => {
@@ -17,13 +16,8 @@ app.get('/download-mp4', (req, res) => {
   }
 
   youtubedl(url, {
-    format: 'bestvideo',
-    dumpSingleJson: true,
-    noWarnings: true,
-    noCallHome: true,
-    noCheckCertificate: true,
-    preferFreeFormats: true,
-    youtubeSkipDashManifest: true
+    format: 'bestvideo+bestaudio',
+    mergeOutputFormat: 'mp4'
   }).then(output => {
     res.json({ url: output.url, title: output.title });
   }).catch(err => {
@@ -31,27 +25,22 @@ app.get('/download-mp4', (req, res) => {
   });
 });
 
-
 app.get('/download-mp3', (req, res) => {
-    const url = req.query.url;
-    if (!url) {
-      return res.status(400).send('URL is required');
-    }
-  
-    youtubedl(url, {
-        format: 'bestaudio',
-        dumpSingleJson: true,
-        noWarnings: true,
-        noCallHome: true,
-        noCheckCertificate: true,
-        preferFreeFormats: true,
-        youtubeSkipDashManifest: true
-      }).then(output => {
-        res.json({ url: output.url, title: output.title });
-      }).catch(err => {
-        res.status(500).send('Failed to download video');
-      });
-    });
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  youtubedl(url, {
+    format: 'bestaudio',
+    extractAudio: true,
+    audioFormat: 'mp3'
+  }).then(output => {
+    res.json({ url: output.url, title: output.title });
+  }).catch(err => {
+    res.status(500).send('Failed to download audio');
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
